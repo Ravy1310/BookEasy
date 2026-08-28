296129fc1c0f3029b628ef6e040f5a3de255d68b
Rafi Isnanto Syahlefi <rafiisnanto2005@gmail.com>
2026-08-28 20:19:44 +0700

    chore: add .gitignore + admin panel migration

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -9,14 +9,26 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
+        protected $fillable =[
+            'name',
+            'email',
+            'password',
+            'reminder_hour'
+        ];
 
+        protected $hidden =[
+            'password',
+            'remember_token',
+        ];
     /**
      * Get the attributes that should be cast.
      *
@@ -27,6 +39,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'reminder_hour' => 'integer',
         ];
     }
 }
